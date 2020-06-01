ee2e8de0b8d4bb03cb4902f1dd966488ba73fb76
VibhaKondur <62919352+VibhaKondur@users.noreply.github.com>
2020-06-01 09:13:27 -0400

    Code first add DB in class exercise

---
+++ CodeFirst.Net_Project/Context/Book.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CodeFirst.Net_Project.Context
+{
+    public class Book
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [MaxLength(50)]
+        public string Title { get; set; }
+        [MinLength(0)]
+        public string Genre { get; set; }
+        [MinLength(0)]
+        public int PageCount { get; set; }
+
+
+        //Context needs 3 things:
+        //1) Subclass DbContext 
+ 
+
+    }
+
+    public class BookContext : DbContext
+    {
+
+        //2) Create a constructor passing in options 
+        public BookContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        //3) A DbSet<model> for each table you want to make
+        public DbSet<Book> Books { get; set; }
+
+    }
+}
---
+++ CodeFirst.Net_Project/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using CodeFirst.Net_Project.Models;
+using CodeFirst.Net_Project.Migrations;
+using CodeFirst.Net_Project.Context;
+
+namespace CodeFirst.Net_Project.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        //private readonly ILogger<HomeController> _logger;
+        private readonly BookContext _context;
+
+        public HomeController(BookContext _context)
+        {
+            this._context = _context;
+        }
+        public IActionResult Index()
+        {
+            List<Book> books = _context.Books.ToList();
+            return View(books);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
